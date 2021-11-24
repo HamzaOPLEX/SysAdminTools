adc4db11c172489db21beb8d2c1f71d68f9793e0
Hamza Alaoui Hamdi <53179016+HamzaOPLEX@users.noreply.github.com>
2021-11-24 08:02:27 -0800

    Update commands.py

--- Commands/windows/commands.py
+++ Commands/windows/commands.py
@@ -2,3 +2,6 @@ import os
 # open network interfaces manager in windows using `ncpa.cpl` command
 os.system('ncpa.cpl')
 #####################################################################
+# get all interfaces mac address using 'getmac /v' command
+os.system('getmac /v')
+#####################################################################
